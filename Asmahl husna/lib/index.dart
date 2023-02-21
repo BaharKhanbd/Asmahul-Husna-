7425873042dacc7e6a18de300ff47636f8f32f70
unknown <baharkhanbd123@gmail.com>
2023-02-22 01:29:13 +0600

    done

--- Asmahl husna/lib/index.dart
+++ Asmahl husna/lib/index.dart
@@ -1,10 +1,9 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:flutter_application_1/Model/PostmodelApi.dart';
-import 'package:http/http.dart' as http;
+
+import 'api_service/asmaul_husna_api.dart';
 
 class Home_Page extends StatefulWidget {
   const Home_Page({super.key});
@@ -14,50 +13,36 @@ class Home_Page extends StatefulWidget {
 }
 
 class _Home_PageState extends State<Home_Page> {
-  Future<PostModel> getPostApi() async {
-    final response =
-        await http.get(Uri.parse("http://api.aladhan.com/asmaAlHusna/1,2"));
-    var data = jsonDecode(response.body.toString());
-    if (response.statusCode == 200) {
-      return PostModel.fromJson(data);
-    } else {
-      return PostModel.fromJson(data);
-    }
-  }
-
+  APIService apiService = APIService();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         leading: Icon(Icons.person),
       ),
-      body: Column(
-        children: [
-          Expanded(
-              child: FutureBuilder<PostModel>(
-                  future: getPostApi(),
-                  builder: (context, snapshot) {
-                    if (snapshot.hasData) {
-                      return ListView.builder(
-                          itemCount: snapshot.data!.data!.length,
-                          itemBuilder: (context, index) {
-                            return Column(
-                              children: [
-                                Text(snapshot.data!.data![index].number
-                                    .toString()),
-                                Text(snapshot.data!.data![index].name
-                                    .toString()),
-                                Text(snapshot.data!.data![index].transliteration
-                                    .toString()),
-                              ],
-                            );
-                          });
-                    } else {
-                      return Text("Loading....");
-                    }
-                  }))
-        ],
-      ),
+      body: FutureBuilder<PostModel>(
+          future: apiService.getPostApi(),
+          builder: (context, AsyncSnapshot<PostModel> snapshot) {
+            if (snapshot.hasData) {
+              return ListView.builder(
+                  scrollDirection: Axis.vertical,
+                  itemCount: snapshot.data!.data!.length,
+                  itemBuilder: (context, index) {
+                    return Column(
+                      children: [
+                        Text(snapshot.data!.data![index].number.toString()),
+                        Text(snapshot.data!.data![index].name.toString()),
+                        Text(snapshot.data!.data![index].transliteration
+                            .toString()),
+                        Text(
+                            snapshot.data!.data![index].en!.meaning.toString()),
+                      ],
+                    );
+                  });
+            } else {
+              return Text("Loading....");
+            }
+          }),
     );
   }
 }
